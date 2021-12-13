b5034de710f5ad589ef66308e60d76cae75e8311
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-14 01:10:18 +0300

    Add sql where statement command file.

---
+++ sql-queries/2.where.sql
@@ -0,0 +1,13 @@
+SELECT * FROM customersinfo
+
+-- Where koþulu
+
+-- Id'si 1 olan kayýt
+SELECT * FROM customersinfo Where id = 1
+
+-- Id'si 10'dan büyük olan kayýtlar
+SELECT * FROM customersinfo Where id>10
+
+-- Id'si 20'den küçük olan kayýtlar
+SELECT * FROM customersinfo Where id<20
+
