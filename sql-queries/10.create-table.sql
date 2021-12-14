0db64a143a2bcd82b2a3c67e4c1514040693b5ef
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-14 12:01:28 +0300

    Use create table command.

---
+++ sql-queries/10.create-table.sql
@@ -0,0 +1,97 @@
+CREATE TABLE africabooks (
+	id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	detail TEXT NOT NULL,
+	price VARCHAR(20),
+	kindOfBook VARCHAR(50),
+	publisher VARCHAR(100),
+	aktif INT
+)
+
+
+CREATE TABLE europeBooks (
+    id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    detail TEXT NOT NULL,
+    price VARCHAR(20),
+    kindOfBook VARCHAR(50),
+    publisher VARCHAR(100),
+    aktif INT
+)
+
+
+CREATE TABLE northAmericaBooks (
+	id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	detail TEXT NOT NULL,
+	price VARCHAR(20),
+	kindOfBook VARCHAR(50),
+	publisher VARCHAR(100),
+	aktif INT
+)
+
+
+CREATE TABLE southAmericaBooks (
+	id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	detail TEXT NOT NULL,
+	price VARCHAR(20),
+	kindOfBook VARCHAR(50),
+	publisher VARCHAR(100),
+	aktif INT
+)
+
+
+CREATE TABLE australiaBooks (
+	id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	detail TEXT NOT NULL,
+	price VARCHAR(20),
+	kindOfBook VARCHAR(50),
+	publisher VARCHAR(100),
+	aktif INT
+)
+
+
+CREATE TABLE caribbeanBooks (
+	id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	detail TEXT NOT NULL,
+	price VARCHAR(20),
+	kindOfBook VARCHAR(50),
+	publisher VARCHAR(100),
+	aktif INT
+)
+
+
+CREATE TABLE centralAmericaBooks (
+	id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	detail TEXT NOT NULL,
+	price VARCHAR(20),
+	kindOfBook VARCHAR(50),
+	publisher VARCHAR(100),
+	aktif INT
+)
+
+
+CREATE TABLE middleEastBooks (
+	id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	detail TEXT NOT NULL,
+	price VARCHAR(20),
+	kindOfBook VARCHAR(50),
+	publisher VARCHAR(100),
+	aktif INT
+)
+
+
+CREATE TABLE antarcticaBooks (
+	id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	detail TEXT NOT NULL,
+	price VARCHAR(20),
+	kindOfBook VARCHAR(50),
+	publisher VARCHAR(100),
+	aktif INT
+)
