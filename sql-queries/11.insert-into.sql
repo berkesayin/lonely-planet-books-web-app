fcf4f11dcb57558a6395d666fe295f2c553e399d
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-14 12:12:04 +0300

    Use insert into statement.

---
+++ sql-queries/11.insert-into.sql
@@ -0,0 +1,12 @@
+INSERT INTO customersinfo (id, name, surname, city, birthdate, email, password, gender)
+VALUES (34, 'Burak', 'Sayin', 'Istanbul', '2009-08-02', 'sayinburak@gmail.com', 'sayinburak', 'Man')
+
+
+INSERT INTO customersinfo (id, name, surname, city, birthdate, email, password, gender)
+VALUES (35, 'Fehmi', 'Ozdemir', 'Istanbul', '1978-08-02', 'fehmiozdemir@gmail.com', 'fehmiozdemir', 'Man')
+
+
+INSERT INTO customersinfo (id, name, surname, city, birthdate, email, password, gender)
+VALUES (36, 'Melek', 'Yilmaz', 'Antalya', '2005-05-02', 'yilmazmelek@gmail.com', 'yilmazmelek', 'Woman')
+
+
