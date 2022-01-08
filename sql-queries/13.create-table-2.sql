50b62e20606d57764bf2bc80d4bf528eee0ee4ab
Berke Sayın <sayinberkesayin@gmail.com>
2022-01-08 13:39:49 +0300

    Add create table query.

---
+++ sql-queries/13.create-table-2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE guides (
+                         id int,
+                         name varchar(255),
+                         region varchar(255),
+                         city varchar(255)
+);
